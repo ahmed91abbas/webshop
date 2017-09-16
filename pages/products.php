0bc45fc47b312608ea6edf5c36e6cab0d09cac86
Carl Dahl <dat14cda@student.lu.se>
2017-09-16 14:22:02 +0200

    Product table added
    Product page filled with some products. Shopping cart connections is yet
    to be implemented but is currently awaiting implementation of the
    shopping cart.

--- pages/products.php
+++ pages/products.php
@@ -1,1 +1,50 @@
-<p>Products</p>
+<div class="center">
+<h1>Our products:</h1><br/>
+<head>
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<table>
+  <tr>
+	<th>Image</th>
+    <th>Product</th>
+    <th>Description</th>
+    <th>Pricing (ZWD)</th>
+  </tr>
+  <tr>
+	<td><img src="https://ih0.redbubble.net/image.105348768.5987/raf,750x1000,075,t,fafafa:ca443f4786.2u1.jpg" alt="Your favorite household item!" style="width:200px;height:200px;"></td>
+    <td>Plumbus</td>
+    <td>Every home needs one!</td>
+    <td>50$</td>
+  </tr>
+  <tr>
+	<td><img src="https://vignette.wikia.nocookie.net/rickandmorty/images/f/f7/Mr._Meeseeks_Box.png/revision/latest?cb=20160909153718" alt="A must for the lazy home owner!" style="width:200px;height:200px;"></td>
+    <td>Meeseeks box</td>
+    <td>An easy solution to all your household needs!</td>
+    <td>1000$</td>
+  </tr>
+  <tr>
+	<td><img src="https://i5.walmartimages.com/asr/e2b2d3f6-96e2-4768-bde4-1e25816db1bc_1.46f313ab982dc06163d83f0e3a2ebc33.jpeg" alt="" style="width:200px;height:200px;"></td>
+    <td>Windex spray</td>
+    <td>A budget alternative to the critically acclaimed Meeseeks box!</td>
+    <td>100$</td>
+  </tr>
+</table>
+<br/><br/></div>
